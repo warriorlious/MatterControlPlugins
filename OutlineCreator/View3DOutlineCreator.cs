2b6ef3b51ee4c2760d8e1e264ca727b085350f0f
larsbrubaker <larsbrubaker@gmail.com>
2014-03-18 10:56:06 -0700

    Updated clipperlib to latest.

--- OutlineCreator/View3DOutlineCreator.cs
+++ OutlineCreator/View3DOutlineCreator.cs
@@ -493,8 +493,8 @@ namespace MatterHackers.MatterControl.Plugins.OutlineCreator
 
                 // now clip the polygons to get the inside and outside polys
                 Clipper clipper = new Clipper();
-                clipper.AddPolygons(lineLoops, PolyType.ptSubject);
-                clipper.AddPolygon(boundingPoly, PolyType.ptClip);
+                clipper.AddPaths(lineLoops, PolyType.ptSubject, true);
+                clipper.AddPath(boundingPoly, PolyType.ptClip, true);
 
                 PolyTree polyTreeForPlate = new PolyTree();
                 clipper.Execute(ClipType.ctIntersection, polyTreeForPlate);
